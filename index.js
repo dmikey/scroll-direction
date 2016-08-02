375e9cef915ea54bd44a7115d6647ce3ecea1587
Derek Anderson <dereanderson@paypal.com>
2016-08-02 10:46:18 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const isDOM = require('is-dom');
 const isFunction = require('is-function');
+const directions = ["UP", "UP/RIGHT", "RIGHT", "DOWN/RIGHT", "DOWN", "DOWN/LEFT", "LEFT", "UP/LEFT", "UP"];
 let prevScrollTop = 0;
 let prevScrollLeft = 0;
 
@@ -16,32 +17,26 @@ module.exports = (element,cb) => {
 
   function getDirection(currentScrollLeft, currentScrollTop, prevScrollLeft, prevScrollTop) {
     
-      // l = Yb - Ya
-      // convert to radians
-      var l = (prevScrollTop - currentScrollTop) * (Math.PI / 180);
-      var prevScrollLeftRad = prevScrollLeft * (Math.PI / 180);
-      var currentScrollLeftRad = currentScrollLeft * (Math.PI / 180);
-      
-      // x = cos θb * sin ∆L
-      var x = Math.cos(prevScrollLeftRad) *  Math.sin(l);
-      
-      // y = cos θa * sin θb – sin θa * cos θb * cos ∆L
-      var y = (Math.cos(currentScrollLeftRad) * Math.sin(prevScrollLeftRad)) 
-      - (Math.sin(currentScrollLeftRad) * Math.cos(prevScrollLeftRad) * Math.cos(l));
-      
-      var r = Math.atan2(x, y);
-      
-      // convert back to degrees
-      r = r * (180 / Math.PI);
-      
-      // directional
-      var directions = ["UP", "UP/RIGHT", "RIGHT", "DOWN/RIGHT", "DOWN", "DOWN/LEFT", "LEFT", "UP/LEFT", "UP"];
+    // l = Yb - Ya
+    // convert to radians
+    let l = (prevScrollTop - currentScrollTop) * (Math.PI / 180);
+    let prevScrollLeftRad = prevScrollLeft * (Math.PI / 180);
+    let currentScrollLeftRad = currentScrollLeft * (Math.PI / 180);
+    
+    // x = cos θb * sin ∆L
+    let x = Math.cos(prevScrollLeftRad) *  Math.sin(l);
+    
+    // y = cos θa * sin θb – sin θa * cos θb * cos ∆L
+    let y = (Math.cos(currentScrollLeftRad) * Math.sin(prevScrollLeftRad)) - (Math.sin(currentScrollLeftRad) * Math.cos(prevScrollLeftRad) * Math.cos(l));
+    
+    // convert back to degrees after finding radians
+    let r = Math.atan2(x, y) * (180 / Math.PI);
 
-      // round and snap to 45 degree angles for 8 way detection
-      var index = Math.floor( ((r - 22.5) % 360) / 45);
+    // round and snap to 45 degree angles for 8 way detection
+    let index = Math.floor(((r - 22.5) % 360) / 45);
 
-      // return scroll direction
-      return directions[index + 1];
+    // return scroll direction
+    return directions[index + 1];
   };
 
   element.addEventListener('scroll',() => { 
